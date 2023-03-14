8e123917fc06de65dc1acc5105104743fdce45de
ZiynatI <ziisomurodova@gmail.com>
2023-03-14 13:48:09 +0500

    DirectionThreadTest.java created

---
+++ src/test/java/org/zi/snake/DirectionThreadTest.java
@@ -0,0 +1,11 @@
+package org.zi.snake;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DirectionThreadTest {
+
+    @Test
+    void run() {
+    }
+}
